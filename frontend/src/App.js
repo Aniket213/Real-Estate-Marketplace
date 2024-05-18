2a80327383b7a49956d3e3193613a50d7b720512
Aniket213 <aniketmittal222@gmail.com>
2024-05-18 15:35:22 +0530

    added router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin'
+import Signout from './pages/Signout'
+import About from './pages/About'
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-red-500'>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/signout' element={<Signout/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
